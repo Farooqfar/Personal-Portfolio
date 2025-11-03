fc249bc8a20928b6516faee1504160d26470a5bd
Farooqfar <info4895@gmail.com>
2025-11-03 06:28:16 +0500

    setting

--- src/app/page.js
+++ src/app/page.js
@@ -1,32 +1,42 @@
 import Navbar from "@/components/applications/Navbar";
 import { Button } from "@/components/ui/button";
 import { RiInstagramFill } from "react-icons/ri";
+import { FaLinkedin } from "react-icons/fa6";
+import Image from "next/image";
 
 export default function Home() {
   return (
     <>
       <main className="bg-black w-full h-screen">
         <Navbar />
-        <main className="p-20">
-          <section>
+        <main className="p-20 flex w-[100%] flex-row max-lg:flex-col">
+          <section className=" p-10 w-[50%] max-lg:w-[100%]">
             <h1 className="text-gray-200 text-2xl">
               Hi I am <br /> Muhammad Farooq
             </h1>
             <h1 className="uppercase text-5xl font-bold text-[#E86600] mt-5">
               website developer
             </h1>
-            <div>
-              <RiInstagramFill />
+            <div className="flex gap-3 mt-5">
+              <a href="#">
+                <RiInstagramFill className="text-red-700 text-xl" />
+              </a>
+              <a href="#">
+                <FaLinkedin className="text-blue-400 text-xl" />
+              </a>
             </div>
             <div className="flex gap-4 mt-5">
-              <button className="bg-[#E66500] text-white w-26 p-1 text-center rounded">
-                <a href="#"> Hire me </a>
-              </button>
+              <a
+                href="#"
+                className="bg-[#E66500] text-center text-white w-26 p-2 rounded transition-all duration-300 hover:scale-90"
+              >
+                Hire me
+              </a>
 
               <a
                 href="#"
                 download
-                className="border-1 border-gray-400 text-white w-32 p-2 text-center rounded hover:bg-[#F86D00] tranistion-all duration-300 hover:border-black"
+                className="border-1 border-gray-400 text-white w-32 p-2 text-center rounded hover:bg-[#F86D00] hover:scale-90 tranistion-all duration-300 hover:border-black"
               >
                 Download CV
               </a>
@@ -38,16 +48,24 @@ export default function Home() {
                 <h2 className="text-white pb-2">Experience</h2>
               </div>
               <div className="bg-[#1C1B1B] w-42 p-3 text-start border-l-2 border-r-2 border-[#383736]">
-                <h1 className="text-[#E66500] text-2xl font-bold pb-2">1+</h1>
-                <h2 className="text-white pb-2 ">Experience</h2>
+                <h1 className="text-[#E66500] text-2xl font-bold pb-2">13+</h1>
+                <h2 className="text-white pb-2 ">Project done</h2>
               </div>
               <div className="bg-[#1C1B1B] w-42 p-3 text-start">
-                <h1 className="text-[#E66500] text-2xl font-bold pb-2">1+</h1>
-                <h2 className="text-white pb-2">Experience</h2>
+                <h1 className="text-[#E66500] text-2xl font-bold pb-2">15+</h1>
+                <h2 className="text-white pb-2">Happy Clients</h2>
               </div>
             </div>
           </section>
-          <section></section>
+          <section className="relative w-[50%] max-lg:w-[100%]">
+            <Image
+              src=""
+              width={100}
+              height={100}
+              alt="Profile"
+              className="absolute"
+            />
+          </section>
         </main>
       </main>
     </>
--- src/components/applications/Navbar.jsx
+++ src/components/applications/Navbar.jsx
@@ -4,21 +4,27 @@ import React from "react";
 function Navbar() {
   return (
     <>
-      <nav className="flex justify-around items-center bg-black text-white">
-        <ul>
-          <Image src="/logo.png" alt="Logo" width={100} height={100} />
+      <nav className="flex justify-around items-center bg-black text-white pt-4">
+        <ul className="flex justify-center items-center">
+          <Image
+            src="/logo.png"
+            alt="Logo"
+            width={100}
+            height={100}
+            className="w-20"
+          />
         </ul>
         <ul className="flex justify-center items-center gap-20">
-          <li className="text-gray-200 text-lg hover:text-[#F86D00] cursor-pointer transition-all duration-300">
+          <li className="relative text-gray-200 text-lg hover:text-[#F86D00] cursor-pointer transition-all duration-300 before:absolute before:content-[''] before:left-0 before:bottom-0 before:w-[0%] hover:before:w-[100%] before:origin-left before:h-[2px] before:bg-[#F86D00] before:transition-all before:duration-300">
             Home
           </li>
-          <li className="text-gray-200 text-lg hover:text-[#F86D00] cursor-pointer transition-all duration-300">
+          <li className="relative text-gray-200 text-lg hover:text-[#F86D00] cursor-pointer transition-all duration-300 before:absolute before:content-[''] before:left-0 before:bottom-0 before:w-[0%] hover:before:w-[100%] before:origin-left before:h-[2px] before:bg-[#F86D00] before:transition-all before:duration-300">
             Project
           </li>
-          <li className="text-gray-200 text-lg hover:text-[#F86D00] cursor-pointer transition-all duration-300">
+          <li className="relative text-gray-200 text-lg hover:text-[#F86D00] cursor-pointer transition-all duration-300 before:absolute before:content-[''] before:left-0 before:bottom-0 before:w-[0%] hover:before:w-[100%] before:origin-left before:h-[2px] before:bg-[#F86D00] before:transition-all before:duration-300">
             About Us
           </li>
-          <li className="text-gray-200 text-lg hover:text-[#F86D00] cursor-pointer transition-all duration-300">
+          <li className="relative text-gray-200 text-lg hover:text-[#F86D00] cursor-pointer transition-all duration-300 before:absolute before:content-[''] before:left-0 before:bottom-0 before:w-[0%] hover:before:w-[100%] before:origin-left before:h-[2px] before:bg-[#F86D00] before:transition-all before:duration-300">
             Resume
           </li>
         </ul>
